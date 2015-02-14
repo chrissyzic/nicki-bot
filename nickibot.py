99876778b904b179fb9c38b692a2b7d0d4f4e29f
chrissyzic <chrissy.ziccarelli@gmail.com>
2015-02-14 15:38:18 -0500

    Update nickibot.py

--- nickibot.py
+++ nickibot.py
@@ -8,12 +8,12 @@ for song in catalog:
     title, stamp_start, stamp_end, spotify, youtube = song
     d[title] = stamp_start, stamp_end, spotify, youtube
 
-inquiry = raw_input('What song do you want to hear Nicki on? ')
-inquiry = inquiry.lower()
+request = raw_input('What song do you want to hear Nicki on? ')
 
 for title in d:
-    if inquiry == title.lower():
-        print "Nicki's verse starts at {0}".format(d[title])
+    if request.lower() == title.lower():
+        #print "Nicki's verse starts at {0} minute and {1} seconds".format(d[title][0][0],d[title][0][2:])
+        print "Here's {0} on youtube: http://youtu.be/{1}?t={2}m{3}s".format(request, d[title][3][32:], d[title][0][0], d[title][0][2:])
 '''
 
 #This looks at a CSV with only two columns (song title and beginning time stamp) because it's less complicated than the full database
