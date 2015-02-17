5a5ef5d4205841feea4b6af1bb7dc24ef22f20b8
chrissyzic <chrissy.ziccarelli@gmail.com>
2015-02-16 23:58:25 -0500

    Update nickibot.py

--- nickibot.py
+++ nickibot.py
@@ -3,30 +3,32 @@
 
 #Import Python's CSV functions
 import csv
+import random
 
-#Open the timestamps spreadsheet as a dictionary called "catalog".
-#For each entry, the song title is saved as the key, and the timestamp data (verse start and verse end) and YouTube link are the values associated with each key.
+#Open the timestamps spreadsheet as a file called "catalog" and create a dictionary called "d"
 catalog = csv.reader(open('timestamps.csv', 'r'))
 d = {}
+
+#In dictionary d, save each song title as a key and the timestamp data (verse start and verse end) and YouTube link as values associated with each key.
 for song in catalog:
     title, stamp_start, stamp_end, spotify, youtube = song
     d[title] = stamp_start, stamp_end, spotify, youtube
 
 #Ask the user what they want to hear.
 #This will evenutally be request = [tweet from user]
-request = raw_input('What song do you want to hear Nicki on? ')
+request = raw_input('What song do you want to hear Nicki on? ')        
 
-#This will give the user a link to a random song Nicki is featured on.
-#if request.lower == "random":
-
-#Match the user's request with a key in the catalog dictionary.
-#Return YouTube link that jumps to Nicki's verse.
-for title in d:
-    if request.lower() == title.lower():
-        if d[title][0] == "0:00":
-            print "Here's Nicki's verse on {0}: {1}".format(title, d[title][3])
-        else:
-            print "Here's Nicki's verse on {0}: http://youtu.be/{1}?t={2}m{3}s".format(request, d[title][3][32:], d[title][0][0], d[title][0][2:])
+#The first part of this If statement will return a random song and corresponding link if the user types "random."
+if request.lower() == "random":
+    rand_song = random.choice(d.keys())
+    print "Here's Nicki's verse on {0}: http://youtu.be/{1}?t={2}m{3}s".format(rand_song, d[rand_song][3][32:], d[rand_song][0][0], d[rand_song][0][2:])
+else:
+    for title in d: #Iterate through all the keys in dictionary "d"
+        if request.lower() == title.lower(): #If the request matches a key, return a YouTube link timestamped for when Nicki's verse starts
+            if d[title][0] == "0:00": #If her verse starts right at the beginning of the song, just share the bare YouTube link
+                print "Here's Nicki's verse on {0}: {1}".format(title, d[title][3])
+            else:
+                print "Here's Nicki's verse on {0}: http://youtu.be/{1}?t={2}m{3}s".format(title, d[title][3][32:], d[title][0][0], d[title][0][2:])
 '''
 
 #This uses a CSV with only two columns (song title and beginning time stamp). It only returns the start time of the verse, does not include a link to the veres on YouTube.
