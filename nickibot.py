0c62b0df8076097ca559557b264bb1f49b9deb2f
chrissyzic <chrissy.ziccarelli@gmail.com>
2015-02-18 22:21:38 -0500

    Update nickibot.py

--- nickibot.py
+++ nickibot.py
@@ -4,30 +4,16 @@
 # -*- coding: utf-8 -*-
  
 import tweepy, time, sys
- 
-#argfile = str(sys.argv[1])
- 
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'Cn43n1RyUrETH7U0C5RbFPF2Y'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'HJAuwmoB2qQHyL3Epf2o3ODVZy1hh2yK5wLPeaxZijjKy01J3C'#keep the quotes, replace this with your consumer secret key
-ACCESS_TOKEN = '3039118167-O9BtuQkx8miSIX37zCBahcJSk638lvHCRsFq8cC'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'd5yBodiwTPgCybFLO3agFJB5OpdiKxxrhdiPI8Zj6JMgi'#keep the quotes, replace this with your access token secret
+
+from nickibot_cred import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-api.update_status(status="get ready world! nicki comin.")
 
-# filename=open(argfile,'r')
-# f=filename.readlines()
-# filename.close()
- 
-# for line in f:
-	# api = tweepy.API(auth)
-	# api.update_status(status=line)
-	# time.sleep(900)#Tweet every 15 minutes
+#Ambitious Version
 
-'''	
-#Import Python's CSV and random functions
+#Import Python's CSV functions
 import csv
 import random
 
@@ -35,28 +21,34 @@ import random
 catalog = csv.reader(open('timestamps.csv', 'r'))
 d = {}
 
-#In dictionary d, save each song title in "catalog" as a key and the timestamp data (verse start and verse end) and YouTube link as values associated with each key.
+#In dictionary d, save each song title as a key and the timestamp data (verse start and verse end) and YouTube link as values associated with each key.
 for song in catalog:
     title, stamp_start, stamp_end, spotify, youtube = song
     d[title] = stamp_start, stamp_end, spotify, youtube
 
 #Ask the user what they want to hear.
-#This will evenutally be request = [tweet from user]
-request = raw_input('What song do you want to hear Nicki on? ')        
+#request = raw_input('What song do you want to hear Nicki on? ')
+
+#Check my @ mentions
+mentions = api.mentions_timeline(count=1)
+
+#Assign twitter handle and tweet contents to their own variables
+for mention in mentions:
+    request = mention.text[17:]
+    requester = mention.user.screen_name
 
 #The first part of this If statement will return a random song and corresponding link if the user types "random."
 if request.lower() == "random":
     rand_song = random.choice(d.keys())
-    print "Here's Nicki's verse on {0}: http://youtu.be/{1}?t={2}m{3}s".format(rand_song, d[rand_song][3][32:], d[rand_song][0][0], d[rand_song][0][2:])
+    api.update_status(status="@{0} Here's Nicki's verse on {1}: http://youtu.be/{2}?t={3}m{4}s".format(requester, rand_song, d[rand_song][3][32:], d[rand_song][0][0], d[rand_song][0][2:]))
 else:
     for title in d: #Iterate through all the keys in dictionary "d"
         if request.lower() == title.lower(): #If the request matches a key, return a YouTube link timestamped for when Nicki's verse starts
-            if d[title][0] == "0:00": #If her verse starts right at the beginning of the song, just share the bare YouTube link with no timestamp
-                print "Here's Nicki's verse on {0}: {1}".format(title, d[title][3])
+            if d[title][0] == "0:00": #If her verse starts right at the beginning of the song, just share the bare YouTube link
+                api.update_status(status="@{0} Here's Nicki's verse on {1}: {2}".format(title, d[title][3]))
             else:
-                print "Here's Nicki's verse on {0}: http://youtu.be/{1}?t={2}m{3}s".format(title, d[title][3][32:], d[title][0][0], d[title][0][2:])
-
-
+                api.update_status(status="@{0} Here's Nicki's verse on {1}: http://youtu.be/{2}?t={3}m{4}s".format(requester, title, d[title][3][32:], d[title][0][0], d[title][0][2:]))
+'''
 
 #This uses a CSV with only two columns (song title and beginning time stamp). It only returns the start time of the verse, does not include a link to the veres on YouTube.
 
