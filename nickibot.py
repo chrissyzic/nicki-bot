158e124f58c3ae5c8cfc7ea3221f5e9c340c4d11
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-03-21 21:26:14 -0400

    Update nickibot.py

--- nickibot.py
+++ nickibot.py
@@ -2,7 +2,7 @@ import os, site, sys
 # Tell wsgi to add the Python site-packages to its path. 
 site.addsitedir('/home/chrissyzic/.virtualenvs/nicki-bot/lib/python2.7/site-packages')
 
-activate_this = os.path.expanduser("~/.virtualenvs/nicki-bot/bin/activate_this.py")
+activate_this = os.path.expanduser("home/chrissyzic/.virtualenvs/nicki-bot/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
 # Calculate the path based on the location of the WSGI script
