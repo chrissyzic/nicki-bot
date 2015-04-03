ebe4578b2612d06f89814d0f9bdd54c77bc7a764
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-04-03 10:50:46 -0400

    Update nickibot.py

--- nickibot.py
+++ nickibot.py
@@ -1,28 +1,23 @@
-print "It's a new day and this is Line 1: Off to a good start."
+#Import all the stuff needed to activate my virtual environment
 import os, site, sys
 
 # Tell wsgi to add the Python site-packages to its path. 
 site.addsitedir('/home/chrissyzic/webapps/findnickisverse/findnickisverse/nickibot-new/lib/python2.7/site-packages')
 
+#Activate my virtual environment
 activate_this = os.path.expanduser("/home/chrissyzic/webapps/findnickisverse/findnickisverse/nickibot-new/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
 # Calculate the path based on the location of the WSGI script
-print "This is Line 11: exec file worked."
 project = '/home/chrissyzic/webapps/findnickisverse/findnickisverse'
-print "This is Line 13: new project path worked"
 workspace = os.path.dirname(project)
-print "This is Line 15: workspace defined OK with new project path"
 sys.path.append(workspace)
-print "This is Line 17: sys.path.append worked"
 
 #Tweepy Authentication
 #Import Tweepy (to authenticate with Twitter), Python's time and sys modules that Tweepy requires to run.
-print "Line 21: everything pre-tweepy worked"
 import tweepy
-print "This is Line 23: import tweepy worked"
 
-#Import credentials to authenticate with Twitter - these are stored in another file because they are SECRET. If they were public, anyone could tweet from my account.
+#Import credentials to authenticate with Twitter - these are stored in another file because they are SECRET. If they were public, anyone could tweet from my account aka BAD NEWS.
 from nickibot_cred import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 print "This is Line 27: successfully imported keys and secrets"
 
@@ -41,7 +36,7 @@ import random
 catalog = csv.reader(open('/home/chrissyzic/webapps/findnickisverse/findnickisverse/nicki-bot/timestamps.csv', 'r'))
 d = {}
 
-#In dictionary d, save each song title as a key and the timestamp data (verse start and verse end) and YouTube link as values associated with each key.
+#In dictionary d, save each song title as a key and the timestamp data (verse start and verse end) and YouTube link as the values associated with each key.
 for song in catalog:
     title, stamp_start, stamp_end, spotify, youtube = song
     d[title] = stamp_start, stamp_end, spotify, youtube
@@ -49,21 +44,22 @@ for song in catalog:
 #Create an empty list to store status IDs
 statuses = []
 
-#Loop through all tweets in my timeline, store the status IDs as a list
+#Loop through all tweets @findnickisverse's has tweeted, store the status IDs as a list
 for status in api.user_timeline():
     statuses.append(status.id)
 
-#Create a variable to hold most recent status ID
+#Create a variable to hold @findnickisverse's most recent tweet's status ID
 most_recent = statuses[0]
 
-#Check for @ mentions since most recent status ID
+#Check for @ mentions since most recent status ID (aka the last time the nickibot tweeted)
+#I do this so that I don't end up sending links to people who have already received a response from the bot
 if most_recent:
     mentions = api.mentions_timeline(since_id=most_recent)
 else:
     mentions = ()
     print "No mentions, early end."
 
-#Loop through list of mentions since last tweet and assign the user's twitter handle and tweet content to their own variables
+#Loop through list of mentions since findnickisverse's last tweet and assign the user/mentioners's twitter handle and tweet content to their own variables
 for mention in mentions:
     request = mention.text[17:]
     requester = mention.user.screen_name
